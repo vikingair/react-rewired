e6396cb4313f3c3514a09598f24bc6ab336dca2c
Viktor Luft <viktor.luft@freiheit.com>
2019-04-07 14:48:11 +0200

    Fixed docs

--- config/webpack.config.app.prod.js
+++ config/webpack.config.app.prod.js
@@ -18,7 +18,7 @@ module.exports = {
     output: {
         path: paths.appBuild,
         filename: 'static/js/[name].[chunkhash:8].js',
-        publicPath: '/morfi',
+        publicPath: '/react-rewired',
     },
     mode: 'production',
     module: {

2734dd6c71c91cbba89dc29fdb571521a2f5060b
Viktor Luft <viktor.luft@freiheit.com>
2019-09-30 16:20:46 +0200

    Fixed TypeScript types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,4 @@
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, ComponentClass } from 'react';
 
 type StoreSetArg<DATA, PD> = PD | ((prev: DATA) => PD);
 type StoreSet<DATA> = (update: StoreSetArg<DATA, Partial<DATA>>) => void;
@@ -7,10 +7,11 @@ type StoreType<DATA> = {
     set: StoreSet<DATA>;
     root: FC<{ children: ReactNode }>;
     wire: <StoreProps, OwnProps>(
-        component: FC<StoreProps & OwnProps>,
+        component: FC<StoreProps & OwnProps> | ComponentClass<StoreProps & OwnProps>,
         storeToProps: (state: DATA) => StoreProps
     ) => FC<OwnProps>;
 };
 type CreateStoreType = <DATA>(data: DATA) => StoreType<DATA>;
+type WiredType = { store: CreateStoreType };
 
-export const createStore: CreateStoreType;
+export const Wired: WiredType;
